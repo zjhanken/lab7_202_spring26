541a566c3379e82954022e41c9f3c2f6d7794596
zjhanken <zjhanken@gmail.com>
2026-05-10 22:58:24 -0700

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -5,16 +5,30 @@ from lab7_1 import MinHeap, insert, extract, heapify_up, heapify_down
 
 class TestMinHeap(unittest.TestCase):
     def test_insert(self):
-        pass
+        heap = MinHeap(heap=[])
+        result = insert(heap, 5)
+        self.assertEqual(result.heap, [5])
 
     def test_insert_multiple(self):
-        pass
+        heap = MinHeap(heap=[])
+        heap = insert(heap, 5)
+        heap = insert(heap, 3)
+        heap = insert(heap, 7)
+        self.assertEqual(heap.heap, [3, 5, 7])
 
     def test_extract(self):
-        pass
+        heap = MinHeap(heap=[1, 5, 3])
+        min_val, new_heap = extract(heap)
+        self.assertEqual(min_val, 1)
+        self.assertEqual(new_heap.heap, [3, 5])
 
     def test_extract_twice(self):
-        pass
+        heap = MinHeap(heap=[1, 5, 3])
+        first_val, heap = extract(heap)
+        second_val, heap = extract(heap)
+        self.assertEqual(first_val, 1)
+        self.assertEqual(second_val, 3)
+        self.assertEqual(heap.heap, [5])
 
     def test_heapify_up(self):
         heap = [5, 10, 8, 20, 15, 9, 1]
